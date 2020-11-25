df102168e9061b96755bdfe70d03d490eeedee96
maxhsu <hsuevaair3317@gmail.com>
2020-11-25 23:47:12 +0800

    20201125-2

--- G3-1/OOP/HW4/lexer.cpp
+++ G3-1/OOP/HW4/lexer.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
+enum Tag :int {
+    AND = 256   ,
+    BASIC       ,
+    BREAK       ,
+    DO          ,
+    ELSE        ,
+    EQ          ,
+    FALSE       ,
+    GE 		    ,
+    ID 		    ,
+    IF 		    ,
+    INDEX 	    ,
+    LE 		    ,
+    MINUS 	    ,
+    NE 		    ,
+    NUM 	    ,
+    OR 		    ,
+    REAL 		,
+    TEMP 		,
+    TRUE 		,
+    WHILE
+};
 class Token{
-
+    public:
+        int w;
 };
 class Word:public Token{
     public:
         string lexeme;
+        Word(string s , int tag){
+            w = tag;
+            lexeme = s;
+        }
 };
 class Lexer{
-    void reserve(Word w){
-        
-    }
+    public:
+        void reserve(Word w){
+            words.insert({w.lexeme , w.w });
+        }
+        Lexer(){
+            reserve(Word("if"   ,   Tag::IF));
+        }
+    private:
+        unordered_map<string , int> words;
 };
